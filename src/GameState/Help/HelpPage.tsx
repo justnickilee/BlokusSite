362ef0bf02604a89054bdfb5998f1f053fa0272d
Nicki Lee <nol3@duke.edu>
2023-08-08 10:23:55 -0600

    #11 Made the help button and section look prettier

--- src/GameState/Help/HelpPage.tsx
+++ src/GameState/Help/HelpPage.tsx
@@ -56,21 +56,24 @@ const HowToPlaySection = () => {
                 your turn, click on a piece in your inventory. It will show up
                 on the board as a green piece, indicating you have not placed it
                 yet. You can now move and rotate your piece around the board
-                using the arrow keys and space bar on your keyboard
-                respectively. At any time, you can click on a different piece in
-                your inventory to move around the board. Once you're happy with
-                the placement and rotation of your piece, click enter on your
-                keyboard to place it. If your piece is in a valid location, it
-                will change from green to your color. Otherwise, an on-screen
-                alert will tell you that it is not a valid move. The board will
-                quickly begin to fill up with pieces from both you and your
-                opponent... this will likely happen before you run out of
-                pieces. When you no longer have any valid moves left, click the
-                "No Moves Remaining" button located below your inventory. Note
-                that once you click this button, your turn will be skipped until
-                the other player finishes placing their pieces or clicks their
-                button. Once both players have clicked the button, the game
-                ends, and the winner is decided!
+                using the <b>arrow keys</b> and <b>space bar</b> on your
+                keyboard respectively. At any time, you may click on a different
+                piece in your inventory to move around the board. Once you're
+                happy with the placement and rotation of your piece, click
+                <b> enter</b> on your keyboard to place it. If your piece is in
+                a valid location, it will change from green to your color.
+                Otherwise, an on-screen alert will tell you that it is not a
+                valid move.
+            </div>
+            <div className={helpCSS.bodyText}>
+                The board will quickly begin to fill up with pieces from both
+                you and your opponent... this will likely happen before you run
+                out of pieces. When you no longer have any valid moves left,
+                click the <b>No Moves Remaining</b> button located below your
+                inventory. Note that once you click this button, your turn will
+                be skipped until the other player finishes placing their pieces
+                or clicks their button. Once both players have clicked the
+                button, the game ends, and the winner is decided!
             </div>
         </div>
     );
@@ -80,16 +83,24 @@ const ValidMovesSection = () => {
     return (
         <div className={helpCSS.helpSubSection}>
             <span className={helpCSS.sectionTitle}>Valid Moves:</span>
-            <div className={helpCSS.bodyText}>
-                Pieces in Blokus have different rules for how they can be placed
-                in relation to opponent's pieces and your own. Every piece you
-                place must touch a corner of one or more of your own pieces;
-                However, the sides of two of your own pieces may not touch. The
-                sides of your piece can touch the sides of any of your
-                opponent's pieces, just not your own. Note that the first move
-                by Player 1 must have a unit placed in the top left corner of
-                the board while the first move by Player 2 must have a unit
-                placed in the bottom right corner.
+            <div className={helpCSS.rule}>
+                <span className={helpCSS.ruleTitle}>Corner Rule: </span>
+                <div className={helpCSS.ruleText}>
+                    Every piece you place must have at least one corner touching
+                    the corner of another one of your pieces. On the first turn
+                    of the game, Player 1 must place a piece with a unit in the
+                    top-left corner of the board while Player 2 must place a
+                    piece with a unit in the bottom-right corner.
+                </div>
+            </div>
+            <div className={helpCSS.rule}>
+                <span className={helpCSS.ruleTitle}>Side Rule: </span>
+                <div className={helpCSS.ruleText}>
+                    You may not place a piece where one of its sides will touch
+                    the side of another of your pieces. Note that the sides of
+                    your pieces are allowed to touch those of your opponent's,
+                    just not your own.
+                </div>
             </div>
         </div>
     );
